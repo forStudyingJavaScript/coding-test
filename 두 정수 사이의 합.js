46bf1d9b13b4f035a1700cdaf612807fac81069e
Liz Chung <lizzhongx@gmail.com>
2024-04-04 04:42:30 +0900

    no message

--- 두 정수 사이의 합.js
+++
@@ -1,6 +0,0 @@
-const solution = (a, b) => (a + b) * ((Math.abs(b - a) + 1) / 2);
-// TC = O(1), SC = O(1)
-
-console.log(solution(3, 5)); //12
-console.log(solution(3, 3)); //3
-console.log(solution(5, 3)); //12

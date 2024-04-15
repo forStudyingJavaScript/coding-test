d1829222502f1a6e2d77acd2447fba5ba369559b
Liz Chung <lizzhongx@gmail.com>
2024-04-15 18:10:50 +0900

    no message

--- week1/배열의 유사도.js
+++
@@ -1,4 +0,0 @@
-const solution = (s1, s2) => s1.filter(n => s2.includes(n)).length;
-
-console.log(solution(['a', 'b', 'c'], ['com', 'b', 'd', 'p', 'c'])); //2
-console.log(solution(['n', 'omg', 'c'], ['m', 'dot'])); //0

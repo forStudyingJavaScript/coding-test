a6e6bd3eee0581106186b514df9563ee1d496ff3
Liz Chung <lizzhongx@gmail.com>
2024-05-01 15:40:39 +0900

    no message

--- week5/qr code.js
+++ week5/qr code.js
@@ -4,5 +4,7 @@ const solution = (q, r, code) => [...code].map((_, i) => i)
                                           .join('');
 // TC = O(n); SC = O(n); 
 
+// const solution = (q, r, code) => [...code].filter((_, i) => i % q === r).join('');
+
 console.log(solution(3, 1, "qjnwezgrpirldywt")); //'jerry'
 console.log(solution(1, 0, "programmers")); //'programmers'

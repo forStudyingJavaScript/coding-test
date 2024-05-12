86a55c7f152d763978071f5988fd9a7c5e39fb43
Liz Chung <lizzhongx@gmail.com>
2024-05-12 16:49:11 +0900

    no message

--- week7/음양 더하기.js
+++
@@ -1,7 +0,0 @@
-function solution(absolutes, signs) {
-  return absolutes.reduce((acc, current, index) =>
-    signs[index] ? acc + current : acc - current, 0);
-}
-
-console.log(solution([4,7,12], [true, false, true])); //9
-console.log(solution([1,2,3], [false,false,true])); //0
--- week7/커피 심부름.js
+++
@@ -1,6 +0,0 @@
-function solution(order) {
-  return order.reduce((acc, current) => acc + (current.includes('cafelatte') ? 5000 : 4500), 0);
-}
-
-console.log(solution(['cafelatte', 'americanoice', 'hotcafelatte', 'anything'])); //19000
-console.log(solution(['americanoice', 'americano', 'iceamericano'])); //13500

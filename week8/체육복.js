f9d49ff1a70d6f58b0151406307d31278f9468ca
Liz-Chung <lizzhongx@gmail.com>
2024-05-24 16:41:31 +0900

    no message

--- week8/체육복.js
+++ week8/체육복.js
@@ -69,3 +69,4 @@ function solution(n, lost, reserve) {
 console.log(solution(5, [2, 4], [1, 3, 5])); // 5
 console.log(solution(5, [2, 4], [3])); // 4
 console.log(solution(3, [3], [1])); // 2
+console.log(solution(7, [2, 3, 4], [1, 2, 3, 6])); //6

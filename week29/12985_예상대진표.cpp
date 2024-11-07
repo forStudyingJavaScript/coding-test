e1ff73d5b68a4ea58f7305e2a68c2eacca3066cd
FrogBaek <rnsdls1996@gmail.com>
2024-11-07 17:59:09 +0900

    week29

---
+++ week29/12985_예상대진표.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int round = 0;
+    while (a != b)
+    {
+        round++;
+        
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
+    }
+
+    return round;
+}
